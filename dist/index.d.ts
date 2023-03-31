58870c26aeccfd2de4fc960fb6f7c0600dd0ce51
Iddriss Raaj <idd.iraaj@gmail.com>
2023-03-31 20:32:42 +0000

    chore: use hosted font

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,5 @@ import "./styles/globals.css";
 import Button from "./components/button";
 import Footer from "./components/footer";
 import theme from "./components/theme";
-export { Button, Footer, theme };
+import * as Utils from "./utils";
+export { Button, Footer, Utils, theme };
